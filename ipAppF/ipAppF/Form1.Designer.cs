640b0c520a19d8e162bd0d4cde4a878b94823560
xvolb <unicartoon2002@gmail.com>
2023-07-17 17:50:51 +0200

    IP
    IP

---
+++ ipAppF/ipAppF/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ipAppF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(767, 23);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(511, 510);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Country";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "IP";
+            this.label2.TextChanged += new System.EventHandler(this.label2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(346, 469);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Write IP";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 58);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Press the button!!!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(578, 446);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 545);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gMapControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "IP Aplication";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ipAppF/ipAppF/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+
+namespace ipAppF
+{
+    public partial class Form1 : Form
+    {
+        private WebClient wc;
+        public Form1()
+        {
+            InitializeComponent();
+            wc = new WebClient();
+            InitializeGMapControl();
+        }
+        private void InitializeGMapControl() //metoda pro nastaveni ovladani google map 
+        {
+            gMapControl1.MapProvider = GMapProviders.GoogleMap;
+            gMapControl1.MinZoom = 5;
+            gMapControl1.MaxZoom = 50;
+            gMapControl1.Zoom = 15;
+            gMapControl1.DragButton = MouseButtons.Left;
+        }
+        private string ExtractValue(string line, string pattern) 
+        {
+            Match match = Regex.Match(line, pattern);
+            return match.Groups[1].Value;
+        }
+        private double ParseDouble(string value)
+        {
+            double result;
+            double.TryParse(value, out result);
+            return result;
+        }
+        private void UpdateLocation(string line) //metoda ve ktere nacitame data z textboxu a pak je pomoci regetu prevadime na vhodny format pro gMap.
+        {
+            line = wc.DownloadString($"http://ipwho.is/{textBox1.Text}?output=xml");
+
+            string country = ExtractValue(line, "<country>(.*?)</country>");
+            string ip = ExtractValue(line, "<ip>(.*?)</ip>");
+            string latitude = ExtractValue(line, "<latitude>((.*?))</latitude>");
+            string longitude = ExtractValue(line, "<longitude>((.*?))</longitude>");
+
+            double la = ParseDouble(latitude);
+            double lo = ParseDouble(longitude);
+
+            label1.Text = country;
+            label2.Text = ip;
+
+            SetMapPosition(la, lo);
+        }
+
+        private void SetMapPosition(double latitude, double longitude) //metoda pro zobrazeni gMap s nastavenou polohou
+        {
+            gMapControl1.Position = new GMap.NET.PointLatLng(latitude, longitude);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string line = "";
+            UpdateLocation(line);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(textBox1.Text, "[^0-9-.]"))
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.TextLength - 1);
+                textBox1.SelectionStart = textBox1.Text.Length;
+            }
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+
+        }
+
+        private void label2_TextChanged(object sender, EventArgs e)
+        {
+            if (label1.Text == String.Empty)
+            {
+                label2.Text = "You wrote incorrect IP";
+            }
+        }
+    }
+}
